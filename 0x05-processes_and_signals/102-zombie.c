426af3a9e2b61e405a0f9f7da39233ca21c1675a
mercylin <linmercymuthoni@gmail.com>
2024-03-02 18:07:57 -0800

    bash script

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+int main(void)
+{
+    pid_t child_pid;
+
+    for (int i = 0; i < 5; i++)
+    {
+        child_pid = fork();
+        if (child_pid > 0)
+        {
+            printf("Zombie process created, PID: %d\n", child_pid);
+            sleep(1);
+        }
+        else
+        {
+            exit(0);
+        }
+    }
+    infinite_while();
+    return (0);
+}
+
